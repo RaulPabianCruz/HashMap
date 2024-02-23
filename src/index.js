d8d8f91246de427a70f5e6cea26b79b2aed371ad
RaulPabianCruz <ito.pabian@gmail.com>
2024-02-23 13:48:17 -0800

    Create a HashMap factory function
    Creates a Node and HashMap factory function to implement the data
    structure.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
+import HashMapFactory from './HashMapFactory.js';
 
+const hashTable = HashMapFactory();
+hashTable.set('Raul', 25);
+hashTable.set('Brienne', 30);
+hashTable.set('Brienne', 29);
+console.log(hashTable.entries());
+console.log(hashTable.length());
